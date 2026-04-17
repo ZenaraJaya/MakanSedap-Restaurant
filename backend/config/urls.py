7bb10a3b0baa91a3afbaf903159892126bb333a5
Zenara Tools <zenaratools@gmail.com>
2026-04-17 09:35:25 +0800

    fix(routing): handle Vercel's stripped /api prefix

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from api.views import root
-
 urlpatterns = [
-    path("", root),
+    path("", include("api.urls")),
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
 ]
